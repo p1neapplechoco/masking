4bfa88278c01b26ba71d07f26378345b4b267574
Nguyen Thien An <ngthienaans@gmail.com>
2023-11-27 04:07:39 +0700

    Update source.py

--- source.py
+++ source.py
@@ -2,17 +2,6 @@ import glob
 import os
 import cv2
 
-def change_brightness(img, value=30):
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    h, s, v = cv2.split(hsv)
-    v = cv2.add(v,value)
-    v[v > 255] = 255
-    v[v < 0] = 0
-    final_hsv = cv2.merge((h, s, v))
-    img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
-    return img
-
-
 inpath = "./datasets/cloth/"
 outpath = "./datasets/mask/"
 
@@ -20,13 +9,14 @@ for file in os.listdir(inpath):
     if file.endswith(".jpg"):
         img = cv2.imread(inpath + file)
         # blur
-        blurred = cv2.blur(img, (15,15))
+        blurred = cv2.blur(img, (20,20))
+        blurred = cv2.blur(blurred, (10,10))
         # grayscale
         gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY) 
         # 1st mask
         ret,masked1 = cv2.threshold(gray,245,255,cv2.THRESH_BINARY_INV)
         # blur the mask
-        mask_blurred = cv2.blur(masked1,(10,10))
+        mask_blurred = cv2.blur(masked1,(15,15))
         # 2nd mask
         ret,masked2 = cv2.threshold(mask_blurred,245,255,cv2.THRESH_BINARY)
 
