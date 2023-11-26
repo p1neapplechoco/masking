2e8daba7e418c0e36449e51718b748ae47e2eaf8
Nguyen Thien An <ngthienaans@gmail.com>
2023-11-27 03:00:52 +0700

    new

--- source.py
+++ source.py
@@ -1,20 +1,22 @@
 import glob
-
+import os
 import cv2
-import ipympl
-import matplotlib.pyplot as plt
-import numpy as np
-import skimage as ski
 
-img = cv2.imread('./datasets/cloth/02783_00.jpg')
+inpath = "./datasets/cloth/"
+outpath = "./datasets/mask/"
 
-img = cv2.blur(img,(15,15))
+for file in os.listdir(inpath):
+    if file.endswith(".jpg"):
+        img = cv2.imread(inpath + file)
+        # blur
+        blurred = cv2.blur(img,(15,15))
+        # grayscale
+        gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY) 
+        # mask
+        ret,masked1 = cv2.threshold(gray,245,255,cv2.THRESH_BINARY_INV)
+        # blur the mask
+        mask_blurred = cv2.blur(img,(10,10))
+        # mask the blur
+        ret,masked2 = cv2.threshold(mask_blurred,245,255,cv2.THRESH_BINARY)
 
-assert img is not None, "file could not be read, check with os.path.exists()"
-thresh1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-ret,img = cv2.threshold(thresh1,245,255,cv2.THRESH_BINARY_INV)
-img = cv2.blur(img,(10,10))
-ret,img = cv2.threshold(img,245,255,cv2.THRESH_BINARY)
-
-
-cv2.imwrite("./datasets/mask/02783_00.jpg", img)
+        cv2.imwrite(outpath + file, masked2)
